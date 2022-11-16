addacd7ea565f9dbced6bfe00de3d0ee5a23788b
Hoamp <setiawanthomas43@gmail.com>
2022-11-16 15:59:58 +0700

    mencoba views

---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Halaman About</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum officia illo sunt! Non alias dolores culpa, numquam ex doloribus vero!</p>    
+        </div>
+    </div>
+</div>
